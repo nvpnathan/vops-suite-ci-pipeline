d28df4399ebbe4524fec031c4792d542dd2e90cb
nvpnathan <nathan79@gmail.com>
2019-10-18 16:24:50 -0700

    added comments

--- tasks/config-vrops/task.sh
+++ tasks/config-vrops/task.sh
@@ -1,11 +1,12 @@
 export ROOT_DIR=`pwd`
 source $ROOT_DIR/vops-suite-ci-pipeline/functions/vrops_functions.bash
-
+####################################################
+## waiting for vm to be ready
 echo "Waiting for VM to Power On"
 while [[ "$(curl -ski -o /dev/null -w ''%{http_code}'' https://$VROPS_IP/casa/node/thumbprint)" != "200" ]];
   do printf '.' sleep 10;
 done
-
+####################################################
 ## get vrops thumbprint
 echo "Retrieving thumbprint"
 VROPS_THUMBPRINT=$(getThumbprint)
